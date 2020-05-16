677eb80e237ba2f9ab051c93339fa3d409ec2ed0
Jake G <jake098877@gmail.com>
2020-05-16 15:35:13 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,18 +1,16 @@
 binding.pry
 class Backer
-  attr_accessor :project
   attr_reader :backed_projects, :name
 
   def initialize(name)
     @name = name
     @backed_projects = []
-    @backers  = backers
   end
 
   def back_project(proj)
     binding.pry
     @backed_projects << proj
-    project.backers << Project.add_backer(proj)
+    Project.backers << proj
 end
 
 end
